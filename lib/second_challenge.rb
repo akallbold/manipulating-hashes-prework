d3ae3da939e95f5304b9c65b17bca6538253064f
Anna Kallenborn-Bolden <a.kallenbornbolden@gmail.com>
2017-11-17 19:37:25 +0000

    automatically backed up by learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -7,7 +7,12 @@ def second_challenge
   }
 
   #code your solution here!
-
+output=[]
+  groceries.each do |key,value|
+    value.each do
+      output << value
+    end
+  end
+  flatten.output
   
-
 end
